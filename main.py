2a9c35c2f1f2269a9b2b6954d4d89ade933892d7
flyingpalli <paskal.thomas@t-online.de>
2025-02-15 22:28:55 +0100

    refactor

--- main.py
+++ main.py
@@ -10,48 +10,37 @@ import torch.optim as optim
 from torch.utils.data import DataLoader
 
 
-def _get_nth_entry(path: os.PathLike, n: int):
-    df = pd.read_csv(
-        filepath_or_buffer=path,
-        dtype=np.float64,
-        engine="c",
-        header=None,
-        skiprows=n - 1,
-        nrows=1,
-    )
-    return torch.tensor(df.values.flatten())
-
-
 class Network(nn.Module):
     def __init__(
         self,
-        device='cpu',
         input_dim=1071,
         output_dim=51,
         hidden_dim=256,
-        n_layers=5,
+        num_layers=5,
         n_heads=8,
         kernel_size=50,
     ):
         super(Network, self).__init__()
-        # self.dropout = nn.Dropout(p=0.2)
-
         self.conv1 = nn.Conv1d(
-            in_channels=1, out_channels=hidden_dim, kernel_size=kernel_size, padding=1
+            in_channels=1,
+            out_channels=hidden_dim,
+            kernel_size=kernel_size,
+            padding=kernel_size // 2,
         )
+        self.conv_bn1 = nn.BatchNorm1d(hidden_dim)
         self.conv2 = nn.Conv1d(
             in_channels=hidden_dim,
             out_channels=hidden_dim,
             kernel_size=kernel_size,
             padding=1,
         )
-        self.conv_bn = nn.BatchNorm1d(hidden_dim)
+        self.conv_bn2 = nn.BatchNorm1d(hidden_dim)
         self.relu = nn.ReLU()
 
         encoder_layer = nn.TransformerEncoderLayer(
             d_model=hidden_dim, nhead=n_heads, dim_feedforward=hidden_dim * 2
         )
-        self.transformer = nn.TransformerEncoder(encoder_layer, num_layers=n_layers)
+        self.transformer = nn.TransformerEncoder(encoder_layer, num_layers=num_layers)
 
         self.fc = nn.Linear(hidden_dim, output_dim)
 
@@ -64,15 +53,15 @@ class Network(nn.Module):
     def forward(self, x):
         # x = self.dropout(x)
 
-        x = x.unsqueeze(1).to(device)
-        x = self.relu(self.conv_bn(self.conv1(x.to(device)).to(device)).to(device)).to(device)
-        x = self.relu(self.conv_bn(self.conv2(x.to(device)))).to(device)
+        x = x.unsqueeze(1)
+        x = self.relu(self.bn1(self.conv1(x)))
+        x = self.relu(self.bn2(self.conv2(x)))
 
         x = x.permute(2, 0, 1)
-        x = self.transformer(x).to(device)
+        x = self.transformer(x)
 
-        x = x.mean(dim=0).to(device)
-        x = self.fc(x).to(device)
+        x = x.mean(dim=0)
+        x = self.fc(x)
         return x
 
         # x = F.sigmoid(self.fc_first(x))
@@ -84,21 +73,24 @@ class Network(nn.Module):
 
 if __name__ == "__main__":
     BATCH_SIZE = 1
-    TRAIN_STEPS = 5000
     LR = 1e-5
     LOSVD = "./data/losvd_split/losvd_scaled"
     SPECTRUM = "./data/spec_split/spec"
 
     torch.set_default_dtype(torch.float64)
-    device = "cuda" if torch.cuda.is_available() else "cpu"
+    if torch.cuda.is_available():
+        device = "cuda"
+        torch.backends.cudnn.benchmark = True
+    else:
+        device = "cpu"
 
     torch.set_default_device(device)
-    torch.multiprocessing.set_start_method('spawn')
+    torch.multiprocessing.set_start_method("spawn")
     print("Using device:", device)
 
     loss_f = nn.MSELoss(reduction="sum")
 
-    network = Network(device=device)
+    network = Network().to(device)
     optimizer = optim.Adam(network.parameters(), lr=LR)
 
     for file in range(20):
@@ -112,11 +104,10 @@ if __name__ == "__main__":
             generator=torch.Generator(device=device),
         )
         losses = 0
-        for i, (x, y) in enumerate(dataloader):
-            y_pred = network(x).to(device)
-            loss = loss_f(y_pred.to(device), y.to(device))
-
+        for i, (x, target) in enumerate(dataloader):
             optimizer.zero_grad()
+            pred = network(x.to(device))
+            loss = loss_f(pred, target.to(device))
             loss.backward()
             optimizer.step()
             print(" step:", file * 50000 + i, "    loss:", loss.item())
