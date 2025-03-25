9b4938f2c9878af092808bfff579678329f86af6
flyingpalli <paskal.thomas@t-online.de>
2025-03-25 20:43:58 +0100

    refactor a couple

---
+++ network.py
@@ -0,0 +1,88 @@
+import logging
+import os
+
+import torch
+import torch.nn as nn
+
+
+class Network(nn.Module):
+    def __init__(
+        self,
+        input_dim=1071,
+        output_dim=51,
+        hidden_dim=256,
+        num_layers=10,
+        n_heads=8,
+        kernel_size=50,
+    ):
+        super(Network, self).__init__()
+        self.conv1 = nn.Conv1d(
+            in_channels=1,
+            out_channels=hidden_dim,
+            kernel_size=kernel_size,
+            padding=kernel_size // 2,
+        )
+        self.bn1 = nn.BatchNorm1d(hidden_dim)
+        self.conv2 = nn.Conv1d(
+            in_channels=hidden_dim,
+            out_channels=hidden_dim,
+            kernel_size=kernel_size,
+            padding=1,
+        )
+        self.bn2 = nn.BatchNorm1d(hidden_dim)
+        self.relu = nn.ReLU()
+
+        encoder_layer = nn.TransformerEncoderLayer(
+            d_model=hidden_dim,
+            nhead=n_heads,
+            dim_feedforward=hidden_dim * 2,
+            batch_first=False,
+        )
+        self.transformer = nn.TransformerEncoder(encoder_layer, num_layers=num_layers, enable_nested_tensor=False)
+
+        self.fc = nn.Linear(hidden_dim, output_dim)
+
+    def forward(self, x):
+        x = x.unsqueeze(1)
+        x = self.relu(self.bn1(self.conv1(x)))
+        x = self.relu(self.bn2(self.conv2(x)))
+
+        x = x.permute(2, 0, 1)
+        x = self.transformer(x)
+
+        x = x.mean(dim=0)
+        x = self.fc(x)
+        return x
+
+
+def save_model_all(run_name: str, step: int, state_dict: dict) -> None:
+    """
+    Save all models and optimizers to a checkpoint file
+    :param run_name: The name of the run
+    :param step: The current step
+    :param state_dict: The current state dictionary
+    :return:
+    """
+    model_folder = f"./models/{run_name}/{step}"
+    os.makedirs(model_folder, exist_ok=True)
+    out_path = f"{model_folder}/checkpoint.pth"
+
+    total_state_dict = {name: obj.state_dict() for name, obj in state_dict.items()}
+    torch.save(total_state_dict, out_path)
+    logging.info(f"Saved all models and optimizers to {out_path}")
+
+
+def load_model_all(state_dict: dict, path: str, device) -> None:
+    """
+    Load all models and optimizers from a checkpoint file
+    :param state_dict: The state dictionary to load the models and optimizers into
+    :param path: The path to the model
+    :param device: The torch device
+    :return:
+    """
+    assert os.path.exists(path), "Path to model does not exist"
+    checkpoint = torch.load(path, map_location=device)
+    for name, obj in state_dict.items():
+        if name in checkpoint:
+            obj.load_state_dict(checkpoint[name])
+    logging.info(f"Models and optimizers loaded from {path}")
---
+++ trecon.py
@@ -0,0 +1,151 @@
+import logging
+import os
+import re
+import time
+
+import duckdb
+import numpy as np
+import pandas
+import tomli
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import typer
+from torch.utils.data import DataLoader
+
+import network
+
+app = typer.Typer()
+
+
+def load_config():
+    with open("config.toml", "rb") as fb:
+        return tomli.load(fb)
+
+
+def read_spec(path: os.PathLike):
+    df = pandas.read_csv(path, sep="\r", header=None, dtype=np.float64).transpose()
+    return torch.tensor(df.values)
+
+
+def write_losvd(y: torch.Tensor, path: os.PathLike):
+    res = [f"{str(val)}\n" for val in y.tolist()[0]]
+    with open(path, mode="w") as f:
+        f.writelines(res)
+
+
+@app.command()
+def evaluate(
+    model_path: str, spec: str, hidden_dim: int = 256, num_layers: int = 10, kernel_size: int = 50, losvd: str = ""
+):
+    logging.basicConfig(
+        format="%(asctime)s %(levelname)s: %(message)s",
+        datefmt="%d/%m/%Y %H:%M:%S",
+        level="INFO",
+    )
+
+    torch.set_default_dtype(torch.float64)
+    if torch.cuda.is_available():
+        device = "cuda"
+        torch.backends.cudnn.benchmark = True
+    else:
+        device = "cpu"
+
+    torch.set_default_device(device)
+    torch.multiprocessing.set_start_method("spawn")
+    logging.info(f"Using device: {device}")
+
+    model = network.Network(
+        hidden_dim=hidden_dim,
+        num_layers=num_layers,
+        kernel_size=kernel_size,
+    ).to(device)
+
+    network.load_model_all({"model": model}, path=model_path, device=device)
+
+    x = read_spec(spec)
+    y = model(x.to(device))
+
+    losvd = losvd
+    if not losvd:
+        losvd = f"{spec}_eval"
+
+    write_losvd(y, losvd)
+
+
+@app.command()
+def train(
+    hidden_dim: int = 256,
+    num_layers: int = 10,
+    kernel_size: int = 50,
+    lr: float = 1e-5,
+    model_path: str = "",
+    batch_size: int = 1,
+):
+    config = load_config()
+
+    run_name = f"tempRecon__{int(time.time())}"
+    logging.basicConfig(
+        format="%(asctime)s %(levelname)s: %(message)s",
+        datefmt="%d/%m/%Y %H:%M:%S",
+        level=config["program"]["log_level"].upper(),
+    )
+
+    try:
+        torch.set_default_dtype(torch.float64)
+        if torch.cuda.is_available():
+            device = "cuda"
+            torch.backends.cudnn.benchmark = True
+        else:
+            device = "cpu"
+
+        torch.set_default_device(device)
+        torch.multiprocessing.set_start_method("spawn")
+        logging.info(f"Using device: {device}")
+
+        loss_f = nn.MSELoss(reduction="sum")
+
+        model = network.Network(
+            hidden_dim=hidden_dim,
+            num_layers=num_layers,
+            kernel_size=kernel_size,
+        ).to(device)
+        optimizer = optim.Adam(model.parameters(), lr=lr)
+
+        file_starts = 0
+        if model_path:
+            match = re.match(r".*tempRecon__\d+/(\d+)/.*", model_path)
+            if match:
+                step = int(match.group(1))
+                file_starts = step // config["data"]["file_size"]
+                network.load_model_all({"model": model}, path=model_path, device=device)
+            else:
+                logging.warning("Could not deduce step from path. Starting from zero.")
+
+        for file in range(file_starts, 20):
+            spectrum = duckdb.read_csv(f"{config['data']['spectrum']}_{file}").df().values
+            losvd = duckdb.read_csv(f"{config['data']['losvd']}_{file}").df().values
+            dataloader = DataLoader(
+                dataset=list(zip(spectrum, losvd)),
+                batch_size=batch_size,
+                num_workers=1,
+                shuffle=True,
+                generator=torch.Generator(device=device),
+            )
+            for i, (x, target) in enumerate(dataloader):
+                current_step = config["data"]["file_size"] * file + i
+                optimizer.zero_grad()
+                pred = model(x.to(device))
+                loss = loss_f(pred, target.to(device))
+                logging.info(f"Step {current_step}, Loss: {loss:.2f}")
+                loss.backward()
+                optimizer.step()
+    except KeyboardInterrupt:
+        logging.warning("KeyboardInterrupt caught! Saving progress...")
+    finally:
+        state_dict = {"model": model}
+        network.save_model_all(run_name, current_step, state_dict)
+
+
+if __name__ == "__main__":
+    app()
